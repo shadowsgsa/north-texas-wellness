77cf190a1cf6498eefbd66f38d1bb5f1c4c298a8
SAUS INTERNATIONAL INC <darth3157@gmail.com>
2026-06-03 19:53:57 +0500

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo2.png";
 import { Button } from "@/components/ui/button";
 import { Facebook, Instagram, Linkedin, CalendarHeart } from "lucide-react";
 import { openBooking } from "@/lib/booking";
