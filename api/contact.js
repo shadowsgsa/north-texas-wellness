4ff537fe11a53bd3373dac5e8d685c51d00baa17
shadowsgsa <darth3157@gmail.com>
2026-06-08 22:27:13 +0500

    testing

--- api/contact.js
+++ api/contact.js
@@ -1,52 +1,39 @@
 import nodemailer from "nodemailer";
 
+
 export default async function handler(req, res) {
-  console.log("API HIT");
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
-  }
+    if (req.method !== "POST") {
+        return res.status(405).json({ success: false, message: "Method not allowed" });
+    }
 
-  console.log("BODY:", req.body);
+    const { name, email, message } = req.body;
 
-  const { name, email, message } = req.body;
+    try {
 
-  if (!name || !email || !message) {
-    return res.status(400).json({ message: "Missing fields" });
-  }
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "darth3157@gmail.com",
+                pass: "lbuqvxhflhrpnbpz",
+            },
+        });
 
+        await transporter.sendMail({
+            from: "darth3157@gmail.com",
+            to: "darth3157@gmail.com",
+            subject: "Zaigham Portfolio | Contact Form",
+            replyTo: email,
+            text: `
+                Name: ${name}
+                Email: ${email}
+                Message: ${message}
+            `,
+        });
 
+        return res.status(200).json({ success: true });
 
-  try {
-    console.log("Creating transporter");
-      const transporter = nodemailer.createTransport({
-        host: "smtp.titan.email",
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-            user: "john@patchmakershub.com",
-            pass: "qwerty@123", 
-        },
-    });
-
-    await transporter.verify();
-  console.log("SMTP verified");
-
-    await transporter.sendMail({
-      from: "Website Contact Form" ,
-      to: "john@patchmakershub.com",
-      subject: "New Contact Form Message",
-      html: `
-        <h2>New Contact Request</h2>
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Message:</strong><br/>${message}</p>
-      `,
-    });
-
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false });
-  }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false });
+    }
 }
