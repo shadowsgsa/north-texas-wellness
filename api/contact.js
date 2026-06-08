b6b87f513459dc2684d0325104010b98f71046a3
shadowsgsa <darth3157@gmail.com>
2026-06-08 22:20:15 +0500

    testing4

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   console.log("API HIT");
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
